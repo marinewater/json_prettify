9475f7b003a8ea94b2aaf46106b1315284a20335
marinewater <martin@bukovics.de>
2019-08-23 20:29:53 +0200

    fixed: default --spaces to 4

--- index.ts
+++ index.ts
@@ -13,14 +13,21 @@ async function main(): Promise<void> {
     const argv = yargs.usage('Usage: $0 file.json [options] or cat file.json | $0 [options]' )
         .command( 'out', 'out file path' )
         .alias( 'o', 'out' )
-        .command( 'spaces', 'amount of spaces for indentation' )
-        .alias( 's', 'spaces' )
+        .command( 'spaces', 'amount of spaces for indentation', {
+            number: {
+                alias: 's',
+                default: 4
+            }
+        })
         .argv;
 
     const input = new Input( argv._.length > 0 ? argv._[ 0 ] : null );
     const json = await input.read();
 
-    const spaces = parseInt( argv.spaces as string, 10 );
+    let spaces = parseInt( argv.spaces as string, 10 );
+    if ( isNaN( spaces ) ) {
+        spaces = 4;
+    }
 
     const output = new Output( jsonPrettify( json, spaces ), argv.out as string );
     await output.write();
