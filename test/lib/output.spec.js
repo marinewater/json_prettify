b725d89ea9a7ef7e330f642ab7870780ca20f0ef
marinewater <martin@bukovics.de>
2019-08-23 14:41:55 +0200

    updated: compile to async await

--- test/lib/output.spec.js
+++ test/lib/output.spec.js
@@ -1,89 +1,30 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var bluebird = require("bluebird");
-var chai_1 = require("chai");
-var fs = require("fs");
-var stdMocks = require('std-mocks');
-var output_1 = require("../../lib/output");
-var readFileAsync = bluebird.promisify(fs.readFile);
+const bluebird = require("bluebird");
+const chai_1 = require("chai");
+const fs = require("fs");
+const stdMocks = require('std-mocks');
+const output_1 = require("../../lib/output");
+const readFileAsync = bluebird.promisify(fs.readFile);
 describe('Output', function () {
     describe('file', function () {
         after(function (done) {
-            fs.unlink('test.json', function (err) { return done(err); });
+            fs.unlink('test.json', err => done(err));
         });
-        it('should write output to a file', function () {
-            return __awaiter(this, void 0, void 0, function () {
-                var output, file_content;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            output = new output_1.Output('{"test": "case"}', 'test.json');
-                            return [4 /*yield*/, output.write()];
-                        case 1:
-                            _a.sent();
-                            return [4 /*yield*/, readFileAsync('test.json')];
-                        case 2:
-                            file_content = _a.sent();
-                            chai_1.assert.strictEqual(file_content.toString(), '{"test": "case"}');
-                            return [2 /*return*/];
-                    }
-                });
-            });
+        it('should write output to a file', async function () {
+            const output = new output_1.Output('{"test": "case"}', 'test.json');
+            await output.write();
+            const file_content = await readFileAsync('test.json');
+            chai_1.assert.strictEqual(file_content.toString(), '{"test": "case"}');
         });
     });
     describe('stdout', function () {
-        beforeEach(function () { return stdMocks.use(); });
-        afterEach(function () { return stdMocks.restore(); });
-        it('should write to stdout', function () {
-            return __awaiter(this, void 0, void 0, function () {
-                var output;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            output = new output_1.Output('{"test": "case"}');
-                            return [4 /*yield*/, output.write()];
-                        case 1:
-                            _a.sent();
-                            chai_1.assert.strictEqual(stdMocks.flush().stdout[0], '{"test": "case"}');
-                            return [2 /*return*/];
-                    }
-                });
-            });
+        beforeEach(() => stdMocks.use());
+        afterEach(() => stdMocks.restore());
+        it('should write to stdout', async function () {
+            const output = new output_1.Output('{"test": "case"}');
+            await output.write();
+            chai_1.assert.strictEqual(stdMocks.flush().stdout[0], '{"test": "case"}');
         });
     });
 });
